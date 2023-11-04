dbead9c43711b9f5b3ba90b110d32e25851ae2b2
Kyle <ktunis08@gmail.com>
2023-11-04 12:11:39 -0700

    Added javascript for main content area

---
+++ script.js
@@ -0,0 +1,191 @@
+
+// Saving homepage innerHTML for use on button1click
+
+const saveDate = document.getElementById('date').innerHTML;
+const saveName = document.getElementById('project-name').innerHTML;
+const saveDescription = document.getElementById('project-description').innerHTML;
+const saveImg = document.getElementById('projectimg').innerHTML;
+
+
+
+//Project 1 Button Event 
+const proj1but = document.getElementById('proj1but');
+
+function button1(event) {
+    document.getElementById('date').innerHTML = saveDate;
+    document.getElementById('project-name').innerHTML = saveName;
+    document.getElementById('project-description').innerHTML = saveDescription;
+    document.getElementById('projectimg').innerHTML = saveImg;
+
+};
+
+proj1but.onclick = button1;
+
+
+//Project 2 Button Event
+const proj2but = document.getElementById('proj2but');
+
+function button2(event) {
+    document.getElementById('date').innerHTML = 'November 14th, 2023';
+    document.getElementById('project-name').innerHTML = 'Project # 2';
+    document.getElementById('project-description').innerHTML = 'TESTING';
+    document.getElementById('projectimg').src = "Images/selfpic.png";
+};
+
+proj2but.onclick = button2;
+
+//Project 3 Button Event
+const proj3but = document.getElementById('proj3but');
+
+function button3(event) {
+    document.getElementById('date').innerHTML = 'November 15th, 2023';
+    document.getElementById('project-name').innerHTML = 'Project # 3';
+    document.getElementById('project-description').innerHTML = 'TESTING';
+    document.getElementById('projectimg').src = "Images/selfpic.png";
+};
+
+proj3but.onclick = button3;
+
+//Project 4 Button Event
+const proj4but = document.getElementById('proj4but');
+
+function button4(event) {
+    document.getElementById('date').innerHTML = 'November 14th, 2023';
+    document.getElementById('project-name').innerHTML = 'Project # 4';
+    document.getElementById('project-description').innerHTML = 'TESTING';
+    document.getElementById('projectimg').src = "Images/selfpic.png";
+};
+
+proj4but.onclick = button4;
+
+//Project 5 Button Event
+const proj5but = document.getElementById('proj5but');
+
+function button5(event) {
+    document.getElementById('date').innerHTML = 'November 14th, 2023';
+    document.getElementById('project-name').innerHTML = 'Project # 5';
+    document.getElementById('project-description').innerHTML = 'TESTING';
+    document.getElementById('projectimg').src = "Images/selfpic.png";
+};
+
+proj5but.onclick = button5;
+
+//Project 6 Button Event
+const proj6but = document.getElementById('proj6but');
+
+function button6(event) {
+    document.getElementById('date').innerHTML = 'November 14th, 2023';
+    document.getElementById('project-name').innerHTML = 'Project # 6';
+    document.getElementById('project-description').innerHTML = 'TESTING';
+    document.getElementById('projectimg').src = "Images/selfpic.png";
+};
+
+proj6but.onclick = button6;
+
+//Project 7 Button Event
+const proj7but = document.getElementById('proj7but');
+
+function button7(event) {
+    document.getElementById('date').innerHTML = 'November 14th, 2023';
+    document.getElementById('project-name').innerHTML = 'Project # 7';
+    document.getElementById('project-description').innerHTML = 'TESTING';
+    document.getElementById('projectimg').src = "Images/selfpic.png";
+};
+
+proj7but.onclick = button7;
+
+//Project 8 Button Event
+const proj8but = document.getElementById('proj8but');
+
+function button8(event) {
+    document.getElementById('date').innerHTML = 'November 14th, 2023';
+    document.getElementById('project-name').innerHTML = 'Project # 8';
+    document.getElementById('project-description').innerHTML = 'TESTING';
+    document.getElementById('projectimg').src = "Images/selfpic.png";
+};
+
+proj8but.onclick = button8;
+
+//Project 9 Button Event
+const proj9but = document.getElementById('proj9but');
+
+function button9(event) {
+    document.getElementById('date').innerHTML = 'November 14th, 2023';
+    document.getElementById('project-name').innerHTML = 'Project # 9';
+    document.getElementById('project-description').innerHTML = 'TESTING';
+    document.getElementById('projectimg').src = "Images/selfpic.png";
+};
+
+proj9but.onclick = button9;
+
+//Project 10 Button Event
+const proj10but = document.getElementById('proj10but');
+
+function button10(event) {
+    document.getElementById('date').innerHTML = 'November 14th, 2023';
+    document.getElementById('project-name').innerHTML = 'Project # 10';
+    document.getElementById('project-description').innerHTML = 'TESTING';
+    document.getElementById('projectimg').src = "Images/selfpic.png";
+};
+
+proj10but.onclick = button10;
+
+//Project 11 Button Event
+const proj11but = document.getElementById('proj11but');
+
+function button11(event) {
+    document.getElementById('date').innerHTML = 'November 14th, 2023';
+    document.getElementById('project-name').innerHTML = 'Project # 11';
+    document.getElementById('project-description').innerHTML = 'TESTING';
+    document.getElementById('projectimg').src = "Images/selfpic.png";
+};
+
+proj11but.onclick = button11;
+
+//Project 12 Button Event
+const proj12but = document.getElementById('proj12but');
+
+function button12(event) {
+    document.getElementById('date').innerHTML = 'November 14th, 2023';
+    document.getElementById('project-name').innerHTML = 'Project # 12';
+    document.getElementById('project-description').innerHTML = 'TESTING';
+    document.getElementById('projectimg').src = "Images/selfpic.png";
+};
+
+proj12but.onclick = button12;
+
+//Project 13 Button Event
+const proj13but = document.getElementById('proj13but');
+
+function button13(event) {
+    document.getElementById('date').innerHTML = 'November 14th, 2023';
+    document.getElementById('project-name').innerHTML = 'Project # 13';
+    document.getElementById('project-description').innerHTML = 'TESTING';
+    document.getElementById('projectimg').src = "Images/selfpic.png";
+};
+
+proj13but.onclick = button13;
+
+//Project 14 Button Event
+const proj14but = document.getElementById('proj14but');
+
+function button14(event) {
+    document.getElementById('date').innerHTML = 'November 14th, 2023';
+    document.getElementById('project-name').innerHTML = 'Project # 14';
+    document.getElementById('project-description').innerHTML = 'TESTING';
+    document.getElementById('projectimg').src = "Images/selfpic.png";
+};
+
+proj14but.onclick = button14;
+
+//Project 15 Button Event
+const proj15but = document.getElementById('proj15but');
+
+function button15(event) {
+    document.getElementById('date').innerHTML = 'November 14th, 2023';
+    document.getElementById('project-name').innerHTML = 'Project # 15';
+    document.getElementById('project-description').innerHTML = 'TESTING';
+    document.getElementById('projectimg').src = "Images/selfpic.png";
+};
+
+proj15but.onclick = button15;
